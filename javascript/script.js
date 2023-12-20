3cc28d4f8368d3d7873f363b2f8e2a2d071c547c
Jhoão <128644543+Jhopn@users.noreply.github.com>
2023-12-20 16:29:44 -0300

    Delete javascript/script.js

--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-let navbar = document.getElementById("navbar");
-window.addEventListener("scroll", () =>{
-    if (window.scrollY  >= 200) {
-        navbar.classList.add("fixed-navbar");
-    } else {
-        navbar.classList.remove("fixed-navbar");
-    }
-});
